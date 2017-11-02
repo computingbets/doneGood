f60da03352fcdd0c4773c9c1c480f3cb66aecbc0
logicmyth <staley77@protonmail.com>
2017-11-02 14:15:00 -0400

    add TTM and json output file

--- tothemarket/allProductsTTM.js
+++ tothemarket/allProductsTTM.js
@@ -8,43 +8,17 @@ var Promise = require('bluebird');
 * This is a two step scraping method. First identify all the pages on a website that list products. Put all of those links into the URL array below
 */
 urls = [
-  'https://www.tothemarket.com/new-arrivals',
+
   'https://www.tothemarket.com/accessories',
-  'https://www.tothemarket.com/accessories/wallets-and-coin-purses',
-  'https://www.tothemarket.com/accessories/makeup-totes',
-  'https://www.tothemarket.com/accessories/scarves',
-  'https://www.tothemarket.com/accessories/tech-accessories',
-  'https://www.tothemarket.com/accessories/bag-accessories',
   'https://www.tothemarket.com/apparel',
   'https://www.tothemarket.com/bags',
-  'https://www.tothemarket.com/bags/clutches',
-  'https://www.tothemarket.com/bags/crossbody-bags',
-  'https://www.tothemarket.com/bags/shoulder-bags',
-  'https://www.tothemarket.com/bags/totes',
-  'https://www.tothemarket.com/bags/backpacks',
-  'https://www.tothemarket.com/bags/travel-bags',
   'https://www.tothemarket.com/jewelry',
-  'https://www.tothemarket.com/jewelry/necklaces',
-  'https://www.tothemarket.com/jewelry/bracelets',
-  'https://www.tothemarket.com/jewelry/earrings',
   'https://www.tothemarket.com/paper-goods',
+  'https://www.tothemarket.com/bath-body',
   'https://www.tothemarket.com/home-goods',
-  'https://www.tothemarket.com/home-goods/baskets',
-  'https://www.tothemarket.com/home-goods/blankets-and-quilts',
-  'https://www.tothemarket.com/home-goods/candles',
-  'https://www.tothemarket.com/home-goods/cooking-accessories',
-  'https://www.tothemarket.com/home-goods/hand-and-tea-towels',
-  'https://www.tothemarket.com/home-goods/holiday',
-  'https://www.tothemarket.com/home-goods/pillows',
-  'https://www.tothemarket.com/home-goods/tabletop',
-  'https://www.tothemarket.com/home-goods/wall-art-and-decor',
   'https://www.tothemarket.com/kids',
-  'https://www.tothemarket.com/pets',
   'https://www.tothemarket.com/mens',
-  'https://www.tothemarket.com/sale',
-  'https://www.tothemarket.com/shoes',
-  'https://www.tothemarket.com/ttm-exclusives',
-  'https://www.tothemarket.com/bath-body'
+  'https://www.tothemarket.com/shoes'
 ]
 
 Promise.map(urls, function(url){
@@ -63,9 +37,11 @@ Promise.map(urls, function(url){
 		// This code here grabs the url from each listing, and then pushes that url into the pageUrls array
 	  $('.product-hover a').each(function(index, elem){
 	    var productUrl = $(elem).attr("href");
+      //console.log(productUrl)
 	    pageUrls.push(productUrl);
 	  });
 	})
+  //console.log(pageUrls);
 	return pageUrls;
 })
 .then(function(pageUrls){
@@ -90,7 +66,7 @@ Promise.map(urls, function(url){
 
 		  // This is where the specific queries are written to get all the info you need
 		  // Can even get all the meta data, google how to get a pages meta data from Jquery
-		  productName = $('product-name h1').text();
+		  productName = $('.product-name h1').text();
 		  productPrice = $('span.price').text();
 		  productDescription = $('div.std').text();
 		  pageTitle = $('title').text();
