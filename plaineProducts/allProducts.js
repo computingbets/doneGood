6a35b7aff2b6f4de03980e0b86f3b249c8acdcee
logicmyth <staley77@protonmail.com>
2018-04-22 09:37:51 -0400

    add plaineProducts w/ keywords for shampoo and conditioner

---
+++ plaineProducts/allProducts.js
@@ -0,0 +1,100 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var rp = require('request-promise');
+var Promise = require('bluebird');
+
+
+/*
+* This is a two step scraping method. First identify all the pages on a website that list products. Put all of those links into the URL array below
+*/
+urls = [
+  'https://www.plaineproducts.com/product-category/products/travel-sample/',
+  'https://www.plaineproducts.com/product-category/products/body-lotion/',
+  'https://www.plaineproducts.com/product-category/products/shampoo/',
+  'https://www.plaineproducts.com/product-category/products/shampoo/page/2/'
+
+]
+
+Promise.map(urls, function(url){
+	var options = {
+		uri: url,
+		transform: function(body){
+			return cheerio.load(body);
+		}
+	}
+	return Promise.delay(50, rp(options));
+}, {concurrency: 1})
+.then(function(pages){
+	pageUrls = [];
+	// Here is the access to the product page listings
+	pages.forEach(function($){
+		// This code here grabs the url from each listing, and then pushes that url into the pageUrls array
+  	  $('.image-fade_in_back').each(function(index, elem){
+	    var productUrl = $(elem).children().first().attr('href');
+      //console.log(productUrl);
+	    pageUrls.push(productUrl);
+	  });
+	})
+	return pageUrls;
+})
+.then(function(pageUrls){
+	Promise.map(pageUrls, function(url){
+		var options = {
+			uri: url,
+			transform: function(body){
+				return [url, cheerio.load(body)];
+			}
+		}
+		return Promise.delay(20, rp(options));
+	}, {concurrency: 5})
+	.then(function(responses){
+		var results = {};
+		// Response Array [url, cheerio body]
+		responses.forEach(function(response){
+			// Now here is where we have access to each individual product page to get the rest of our information
+		  var productUrl, productPrice, imageUrl, pageTitle, productDescription, productName, keywords
+
+		  productUrl = response[0];
+		  $ = response[1];
+
+		  // This is where the specific queries are written to get all the info you need
+		  // Can even get all the meta data, google how to get a pages meta data from Jquery
+      keywords = [];
+      //$('.selDiv option[value="SEL1"]')
+      key1 = $('#pa_scent option[value=rosemary-mint-vanilla]').text();
+      key2 = $('#pa_scent option[value=citrus-lavender]').text();
+		  productName = $('.product-title').text();
+      //$('p.price > span').text();
+      productDescription = $('.product-short-description').text();
+      productPrice = $('.price').text();
+      pageTitle = $('title').text();
+		  imageUrl = $('woocommerce-product-gallery__image slide.first is-selected').children().first().attr('href');
+
+      keywords.push(key1, key2);
+      // Store all the info we found into the results array
+		  results[productName] = {
+		    'productName': productName,
+		    'productPrice': productPrice,
+		    'productDescription': productDescription,
+		    'productUrl': productUrl,
+		    'pageTitle': pageTitle,
+		    'imageUrl': imageUrl,
+        'keywords': keywords
+		  };
+
+		})
+		return results;
+	})
+	.then(function(results){
+		// Now write the results to a json file
+    fs.writeFile('output.json', JSON.stringify(results, null, 4), function(err){
+      console.log('done');
+    })
+	})
+	.catch(function(err){
+		console.log(err);
+	})
+})
+.catch(function(err){
+	console.log(err);
+})
